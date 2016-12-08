3a9f18cd752c5ff99fc844ea9b506e87ae87eeed
WmfSystem <willianmarquesfreire@gmail.com>
2016-12-07 22:13:07 -0200

    Fim Curso

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,15 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     jshint = require('gulp-jshint'),
     jshintStylish = require('jshint-stylish'),
-    cssLint = require('gulp-csslint')
+    cssLint = require('gulp-csslint'),
+    autoprefixer = require('gulp-autoprefixer'),
+    less = require('gulp-less')
 
-gulp.task('default', ['copy'], function() {
+gulp.task('default', ['copy'], function () {
     gulp.start('build-img', 'usemin')
 })
 
-gulp.task('server', function() {
+gulp.task('server', function () {
     browserSync.init({
         server: {
             baseDir: 'src'
@@ -23,28 +25,38 @@ gulp.task('server', function() {
     })
 
     gulp.watch('src')
-        .on('change', function() {
+        .on('change', function () {
             browserSync.reload()
         })
 
     gulp.watch('src/js/*.js')
-        .on('change', function(event) {
+        .on('change', function (event) {
             gulp.src(event.path)
                 .pipe(jshint.reporter(jshintStylish))
         })
 
     gulp.watch('src/css/*.css')
-        .on('change', function(event) {
+        .on('change', function (event) {
             gulp.src(event.path)
                 .pipe(cssLint.reporter())
         })
+
+    gulp.watch('src/less/*.less')
+        .on('change', function (event) {
+            gulp.src(event.path)
+                .pipe(less().on('error', function(error) {
+                    console.log('Problema na compilação do LESS.')
+                    console.log(error.message)
+                }))
+                .pipe(less(gulp.dest('src/css')))
+        })
 })
 
-gulp.task('usemin', function() {
+gulp.task('usemin', function () {
     gulp.src('dist/**/*.html')
         .pipe(usemin({
             'js': [uglify],
-            'css': [cssmin]
+            'css': [autoprefixer, cssmin]
         }))
         .pipe(gulp.dest('dist'))
 })
@@ -52,7 +64,7 @@ gulp.task('usemin', function() {
 /**
  *  Substituido build-html e build-js por usemin
  */
-gulp.task('build-html', function() {
+gulp.task('build-html', function () {
     gulp.src('dist/**/*.html')
         .pipe(htmlReplace({
             js: 'all.js'
@@ -60,26 +72,25 @@ gulp.task('build-html', function() {
         .pipe(gulp.dest('dist'))
 })
 
-gulp.task('build-js', function() {
+gulp.task('build-js', function () {
     gulp.src(['dist/js/script1.js', 'dist/js/script2.js'])
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'))
 })
 
-gulp.task('build-img', function() {
+gulp.task('build-img', function () {
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'))
 })
 
-gulp.task('copy', ['clean'], function() {
+gulp.task('copy', ['clean'], function () {
     return gulp.src('src/**/*')
         .pipe(gulp.dest('dist'))
 })
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return gulp.src('dist')
         .pipe(clean())
 })
-
